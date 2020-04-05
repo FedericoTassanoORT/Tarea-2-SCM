31c15420d6bceb94df5a20740c9fa92e8cbbb7b9
unknown <fedetas@hotmail.com>
2020-04-05 17:46:26 -0300

    commit fase A de tarea

---
+++ Usuario.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ratein
+ */
+public class Usuario {
+    private String Nombre;
+    private int cedula;
+    private int contrasena;
+    private int edad;
+    private int CantidadHijos;
+    private int saldo;
+    
+}
